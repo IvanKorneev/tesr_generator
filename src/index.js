c8e2d6348dbeb4353287ffc467309efab2aa889a
Ivan <bomak54@gmail.com>
2021-11-11 11:38:17 +0200

    add react redux add store,rootReducer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import App from "./component/app";
 import ErrorBoundry from "./component/error-boundry";
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from "react-redux";
+import store from "./store/store";
 
 
 ReactDOM.render(
-    <ErrorBoundry>
-        <Router>
-            <App/>
-        </Router>
-    </ErrorBoundry>
+    <Provider store={store}>
+        <ErrorBoundry>
+            <Router>
+                <App/>
+            </Router>
+        </ErrorBoundry>
+    </Provider>
     , document.getElementById('root')
 );
---
+++ src/store/reducers/testReducer.js
@@ -0,0 +1,7 @@
+const initialState = {
+    test:[1,2,3,4,5]
+}
+export const testReducer = (state = initialState, action) => {
+    return state
+
+}
